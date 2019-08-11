bb97d4fda4bb9f9d5cdea3060f657d75c271453d
Kabir S. Tamari <kabirtamari@gmail.com>
2019-08-11 19:56:25 +0545

    menu logic working, needs beaufifying

--- LiveOrder/urls.py
+++ LiveOrder/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('ApiModule.urls')),
-    path('reception/', include('ReceptionModule.urls'))
+    path('reception/', include('ReceptionModule.urls')),
+    path('menu/', include('MenuModule.urls'))
 ]
--- MenuModule/views.py
+++ MenuModule/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from ApiModule.models import FoodItem, FoodType
+import json 
 
-# Create your views here.
+
+def menu(request):
+    Food = FoodItem.objects.all()
+    food_list = {}
+    Foodtype = FoodType.objects.all()
+    for typename in Foodtype:
+        food_list[str(typename.food_type)] = {'name':[], 'price':[]}
+    for f in Food :
+        if(f.is_active):
+            food_list[str(f.food_type)]['name'].append(str(f.name))
+            food_list[str(f.food_type)]['price'].append(str(f.price))
+    return render(request, 'MenuModule/menu.html', {'data':json.dumps(food_list)})
