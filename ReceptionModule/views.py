789d8680b4b6f08e9754f7784ee55af6643b0f4f
Kabir S. Tamari <kabirtamari@gmail.com>
2019-08-14 13:40:41 +0545

    req added

--- ReceptionModule/views.py
+++ ReceptionModule/views.py
@@ -13,11 +13,12 @@ def menu(request):
 
 def reception(request):
     orders_json = {'orders':[]} 
-    orders = Order.objects.filter(state = "PENDING").order_by('timestamp')[:20].values('id','state','timestamp','table_number')
+    # orders = Order.objects.filter(state = "PENDING").order_by('timestamp')[:20].values('id','state','timestamp','table_number')
+    orders = Order.objects.filter(state="PENDING").order_by('timestamp')[:20]
     print(orders)
-    # for order in orders:
-    #     food_items = 
-    #     # ser_order = serializers.serialize('json',[order,])
-    #     orders_json['orders'].append(order)
-    # print(orders_json)
+    for order in orders:
+        # food_items = 
+        # ser_order = serializers.serialize('json',[order,])
+        orders_json['orders'].append(order)
+    print(orders_json)
     return render(request, 'ReceptionModule/reception.html', {'data':orders_json})
