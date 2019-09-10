fbec30bbb024a9d632913008312dc6dc7523b27c
Kabir Tamari <rabbit.bonex@gmail.com>
2019-09-11 05:03:47 +0545

    added usercreation and form with error shownn

--- AdminModule/forms.py
+++ AdminModule/forms.py
@@ -11,11 +11,13 @@ class LoginForm(forms.Form):
     class Meta: 
         fields = ['username','password']
 class UserForm(UserCreationForm,ModelForm):
-    username = forms.CharField(min_length=3, max_length=30)
-    email = forms.EmailField(required = True)
-    first_name = forms.CharField(required =  True)
-    last_name = forms.CharField(required =True)
-
+    
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ['first_name','last_name','email','username']
+
+
+# username = forms.CharField(min_length=3, max_length=30)
+#     email = forms.EmailField(required = True)
+#     first_name = forms.CharField(required =  True)
+#     last_name = forms.CharField(required =True)
--- AdminModule/urls.py
+++ AdminModule/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_panel, name = "Main admin Panel"),
-    path('user/register',views.user_registration, name = "new user registration"),
-    path('user/manage', views.user_manager, name = "User manager"),
 ]
